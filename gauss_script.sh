22c8921e036d01a85977c3306c9b4eb9a6f58056
Amy <amy.o-keefe.2020@uni.strath.ac.uk>
2026-04-02 22:09:26 +0100

    modified submission script suitable for the 8-core calculation

--- gauss_script.sh
+++ gauss_script.sh
@@ -3,15 +3,15 @@
 #SBATCH --partition=teaching
 #SBATCH --account=teaching
 #SBATCH --distribution=cyclic
-#SBATCH --ntasks=4 --nodes=1
+#SBATCH --ntasks=8 --nodes=1
 #SBATCH --time=01:00:00
-#SBATCH --job-name=gauss_2cores
+#SBATCH --job-name=gauss_8cores
 #SBATCH --output=slurm-%j.out
 module purge
 module load gaussian/g16
 /opt/software/scripts/job_prologue.sh
 export GAUSS_SCRDIR=$SLURM_SUBMIT_DIR
-g16 < Molecule_ANTCEN.com > Molecule_ANTCEN_4cores.log
-g16 < Molecule_PENCEN.com > Molecule_PENCEN_4cores.log
-g16 < Molecule_TETCEN.com > Molecule_TETCEN_4cores.log
+g16 < Molecule_ANTCEN.com > Molecule_ANTCEN_8cores.log
+g16 < Molecule_PENCEN.com > Molecule_PENCEN_8cores.log
+g16 < Molecule_TETCEN.com > Molecule_TETCEN_8cores.log
 /opt/software/scripts/job_epilogue.sh
